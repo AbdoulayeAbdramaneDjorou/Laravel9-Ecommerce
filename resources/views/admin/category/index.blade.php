90d67a164ccb5d690c40a603db4973d43ee73158
AbdoulayeAbdramaneDjorou <abdoulaye.djorumi@gmail.com>
2023-04-24 22:20:54 +0300

    route change

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -11,12 +11,13 @@
        <div class="container-fluid">
          <div class="row mb-2">
            <div class="col-sm-6">
-             <h1>Category List</h1>
+           <a href="/admin/category/create" class="btn btn-block btn-info" style="width:200px">Add Category</a>
+
            </div>
            <div class="col-sm-6">
              <ol class="breadcrumb float-sm-right">
-               <li class="breadcrumb-item"><a href="#">Home</a></li>
-               <li class="breadcrumb-item active">Blank Page</li>
+               <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+               <li class="breadcrumb-item active">Category List</li>
              </ol>
            </div>
          </div>
@@ -28,27 +29,56 @@
 
        <!-- Default box -->
        <div class="card">
-         <div class="card-header">
-           <h3 class="card-title">Title</h3>
+                     <div class="card-header">
 
-           <div class="card-tools">
-             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-               <i class="fas fa-minus"></i>
-             </button>
-             <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-               <i class="fas fa-times"></i>
-             </button>
-           </div>
-         </div>
-         <div class="card-body">
-           Start creating your amazing application!
-         </div>
-         <!-- /.card-body -->
-         <div class="card-footer">
-           Footer
-         </div>
-         <!-- /.card-footer-->
-       </div>
+                       <h3 class="card-title">Category List</h3>
+                     </div>
+                     <!-- /.card-header -->
+                     <div class="card-body">
+                       <table class="table table-bordered">
+                         <thead>
+                           <tr>
+                             <th style="width: 10px">Id</th>
+                             <th>Title</th>
+                             <th>Keywords</th>
+                             <th>Description</th>
+                             <th>Image</th>
+                             <th>Satus</th>
+                             <th style="width: 40px">Edit</th>
+                             <th style="width: 40px">Delete</th>
+                             <th style="width: 40px">Show</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                         @foreach($data as $rs)
+                           <tr>
+                             <td>{{$rs->id}}</td>
+                             <td>{{$rs->title}}</td>
+                             <td>{{$rs->keywords}}</td>
+                             <td>{{$rs->description}}</td>
+                             <td>{{$rs->image}}</td>
+                             <td>{{$rs->status}}</td>
+                             <td><a href="/admin/category/edit/{{$rs->id}}" class="btn btn-block btn-info btn-sm">Edit</a></td>
+                             <td><a href="/admin/category/delete/{{$rs->id}}" class="btn btn-block btn-danger btn-sm">Delete</a></td>
+                             <td><a href="/admin/category/show/{{$rs->id}}" class="btn btn-block btn-success btn-sm">Show</a></td>
+
+                           </tr>
+
+                         @endforeach
+                         </tbody>
+                       </table>
+                     </div>
+                     <!-- /.card-body -->
+                     <div class="card-footer clearfix">
+                       <ul class="pagination pagination-sm m-0 float-right">
+                         <li class="page-item"><a class="page-link" href="#">«</a></li>
+                         <li class="page-item"><a class="page-link" href="#">1</a></li>
+                         <li class="page-item"><a class="page-link" href="#">2</a></li>
+                         <li class="page-item"><a class="page-link" href="#">3</a></li>
+                         <li class="page-item"><a class="page-link" href="#">»</a></li>
+                       </ul>
+                     </div>
+                   </div>
        <!-- /.card -->
 
      </section>
